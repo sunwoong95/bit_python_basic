fce23b384f3a6c05005f3da3f8045dd4379c261f
sunwoong95 <qwerty951224@gmail.com>
2022-11-01 15:02:50 +0900

    Study_for

--- Q3.py
+++ Q3.py
@@ -1,19 +1,34 @@
 # and or
 # and 둘다 만족.
 # or 둘중 하나 또는 둘다 만족.
-# 12
-a = 15
 
-if (a%2==0 and a%3==0):
-    print(f"{a}는 2와 3의 공배수 입니다.")
-elif (a%2 > 0 or a%3 > 0):
-    print(f"{a}는 2와 3의 공배수가 아닙니다.")
-    if (a%2 == 0):
-        print(f"{a}는 2의 배수입니다.")
-    elif (a%3 == 0):
-        print(f"{a}는 3의 배수입니다.")
+# a = 1278
+
+# answer = f"{a}는"
+
+# if (a%2==0 and a%3==0):
+#     answer += " 2와 3의 공배수 입니다"
+# elif (a%2 == 0):
+#       answer += " 2의 배수 입니다"
+# elif (a%3 == 0):
+#       answer += " 3의 배수 입니다"
+# else:
+#    answer += " 2와 3의 공배수가 아닙니다"
+
+# print(answer)
 
 # print(f"{a}는 2와 3의 공배수 입니다.")
 # print(f"{a}는 2와 3의 공배수가 아닙니다.")
 # print(f"{a}는 2의 배수입니다.")
 # ptint(f"{a}는 3의 배수입니다.")
+
+# 여자친구가 떡볶이랑 아이스크림이 먹고싶다.
+# if 떡볶이 and 아이스크림:
+#     print("혼났어요")
+# elif 떡볶이:
+#     print("아이스크림")
+# elif 아이스크림:
+#     print("떡볶이")
+# else:
+#     print("아무것도 혼남")
+# print("혼났어요")
