83b83e2416113f0203ec0d0279a47d71cf9cc2a5
sunwoong95 <qwerty951224@gmail.com>
2022-11-01 15:01:50 +0900

    Study_for

--- Q4.py
+++ Q4.py
@@ -2,8 +2,8 @@
 # a = [10,20,30,10,20,30,10,20,30]
 
 # 디버그-txt 참조
-
-# for 변수 in range():
+# for i in a = 통으로 진행
+# for 변수 in range(): = 하나씩 까서 진행
 # range(start,stop[,step])
 # range(0,len(a)) 0, 1, 2
 # 0 <= i < 3
@@ -17,10 +17,25 @@
 #     print(i)
 # print(a)
 
+# a = [5,8,7,9,10]
+# #a.sort()
+# for i in range(0,len(a)):
+#     x = a[i]
+#     if(a[i]%2==0):
+#         print(f"{x}는 짝수 입니다.")
+#     elif(a[i]%2>0):
+#         print(f"{x}는 홀수 입니다.")
+
 a = [5,8,7,9,10]
-for i in range(0,len(a)):
-    x = a[i]
-    if(a[i]%2==0):
-        print(f"{x}는 짝수 입니다.")
-    elif(a[i]%2>0):
-        print(f"{x}는 홀수 입니다.")
+
+# i = index의 약자.
+# indexof() 안배움.
+# el = element의 약자.
+# fot el element
+# a a[index]
+for el in a:
+#print(el)
+    if(el%2==0):
+         print(f"{el}는 짝수 입니다.")
+    elif(el%2>0):
+         print(f"{el}는 홀수 입니다.")
